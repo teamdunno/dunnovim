b8599b561831f9bd5c3e42e9b82cf8669989561c
Teesh <88670913+teesh3rt@users.noreply.github.com>
2024-06-11 20:36:46 +0300

    [init] my nvim config thus far

---
+++ init.lua
@@ -0,0 +1,18 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+vim.o.clipboard = "unnamedplus"
+vim.o.number = true
+vim.o.shiftwidth = 4
+
+require("lazy").setup("plugins")
---
+++ lua/plugins/init.lua
@@ -0,0 +1,48 @@
+return {
+    {
+	"AlphaTechnolog/pywal.nvim",
+	as = "pywal",
+	colorscheme = "pywal",
+	-- config = function() vim.cmd.colorscheme("pywal") end
+    },
+    {
+	"folke/tokyonight.nvim",
+	colorscheme = "tokyonight",
+	config = function() vim.cmd.colorscheme("tokyonight") end
+    },
+    {
+	"nvim-telescope/telescope.nvim",
+	cmd = "Telescope",
+	keys = {
+	    { " ff", ":Telescope find_files<cr>" }
+	},
+	dependencies = "nvim-lua/plenary.nvim"
+    },
+    {
+	"ggandor/leap.nvim",
+	keys = { "s", "S" },
+	config = function() require("leap").create_default_mappings() end
+    },
+    {
+	"nvim-lualine/lualine.nvim",
+	opts = { options = { theme = "pywal-nvim" } }
+    },
+    {
+	"akinsho/toggleterm.nvim",
+	opts = {},
+	keys = {
+	    { " t", ":ToggleTerm<cr>" }
+	}
+    },
+    {
+	"tpope/vim-fugitive",
+	cmd = "Git",
+	keys = {
+	    { " g", ":Git<cr>" }
+	}
+    },
+    {
+	"lewis6991/gitsigns.nvim",
+	opts = {}
+    }
+}
