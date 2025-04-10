91487c2df6cc3a092b5ab4321587a17c64f23385
teesh3rt <ilaylevy611@gmail.com>
2025-04-10 16:02:13 +0300

    feat: switch to a new bufferline plugin

---
+++ lua/core/plugins/bufferline.lua
@@ -0,0 +1,12 @@
+return {
+    'akinsho/bufferline.nvim',
+    version = "*",
+    dependencies = 'nvim-tree/nvim-web-devicons',
+    opts = {},
+    event = "VeryLazy",
+    keys = {
+        { "<Tab>",   "<cmd>bnext<cr>" },
+        { "<S-Tab>", "<cmd>bprev<cr>" },
+        { "<A-w>",   "<cmd>bdelete<cr>" }
+    }
+}
