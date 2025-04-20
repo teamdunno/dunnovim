6a20b87ec57b9d9b7e8cfb155bfa7a11cfb354a3
teesh3rt <ilaylevy611@gmail.com>
2025-04-20 22:44:44 +0300

    feat: make bufferline feel more natural

--- lua/core/plugins/bufferline.lua
+++ lua/core/plugins/bufferline.lua
@@ -2,11 +2,30 @@ return {
     "akinsho/bufferline.nvim",
     version = "*",
     dependencies = "nvim-tree/nvim-web-devicons",
-    opts = {},
+    config = function()
+        local bufferline = require("bufferline")
+        bufferline.setup({})
+
+        -- Set Alt+1 through Alt+9 to switch to buffers 1–9
+        for i = 1, 9 do
+            vim.keymap.set("n", "<A-" .. i .. ">", function()
+                bufferline.go_to(i, true)
+            end, { desc = "Go to buffer " .. i })
+        end
+    end,
     event = "VeryLazy",
-    keys = {
-        { "<Tab>", "<cmd>bnext<cr>" },
-        { "<S-Tab>", "<cmd>bprev<cr>" },
-        { "<A-w>", "<cmd>bdelete<cr>" },
-    },
+    keys = (function()
+        local keys = {
+            { "<Tab>", "<cmd>bnext<cr>", desc = "Next buffer" },
+            { "<S-Tab>", "<cmd>bprev<cr>", desc = "Previous buffer" },
+            { "<A-w>", "<cmd>bdelete<cr>", desc = "Close buffer" },
+        }
+
+        -- Add Alt+1 through Alt+9 to keys list
+        for i = 1, 9 do
+            table.insert(keys, { "<A-" .. i .. ">", desc = "Go to buffer " .. i })
+        end
+
+        return keys
+    end)(),
 }
