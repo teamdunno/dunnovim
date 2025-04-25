f5dda9e24170a709fe4fad346e3f9f75f573af56
teesh3rt <ilaylevy611@gmail.com>
2025-04-25 17:11:15 +0300

    feat: add venv switcher

--- lua/dunnovim/plugins/extras/lang/python.lua
+++ lua/dunnovim/plugins/extras/lang/python.lua
@@ -21,4 +21,21 @@ return {
             return opts
         end,
     },
+
+    {
+        "linux-cultist/venv-selector.nvim",
+        dependencies = {
+            "neovim/nvim-lspconfig",
+            -- { "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
+        },
+        branch = "regexp", -- This is the regexp branch, use this for the new versions
+        ft = "python",
+        keys = {
+            { ",v", "<cmd>VenvSelect<cr>" },
+        },
+        ---@type venv-selector.Config
+        opts = {
+        },
+        cond = vim.loop.os_uname().sysname ~= "Windows_NT"
+    },
 }
