2896148e72a2704b913037639e70a7c8279e43a7
teesh3rt <ilaylevy611@gmail.com>
2025-04-26 13:46:56 +0300

    fix: cache settings upon first load

--- lua/dunnovim/utils/config.lua
+++ lua/dunnovim/utils/config.lua
@@ -14,13 +14,20 @@ M.settings = {
     extras = {},
 }
 
+M.is_default = true
+
 ---Get the user's settings
 ---@return dunnovim.Settings settings The user's settings
 function M.get_config()
+    if not M.is_default then
+        return M.settings
+    end
+
     ---@type dunnovim.Settings
     local user_settings = require("custom.settings")
 
     M.settings = vim.tbl_deep_extend("force", M.settings, user_settings)
+    M.is_default = false
 
     return M.settings
 end
