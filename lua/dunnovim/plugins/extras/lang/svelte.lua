7e032c89bb2e1b61772890776fac9f61866ff3a6
teesh3rt <ilaylevy611@gmail.com>
2025-05-01 22:03:37 +0300

    feat: add svelte support

---
+++ lua/dunnovim/plugins/extras/lang/svelte.lua
@@ -0,0 +1,21 @@
+return {
+    {
+        "neovim/nvim-lspconfig",
+        ft = { "svelte" },
+        opts = {
+            servers = {
+                svelte = {},
+            },
+        },
+    },
+
+    {
+        "stevearc/conform.nvim",
+        ft = { "svelte" },
+        opts = function(_, opts)
+            opts.formatters_by_ft = opts.formatters_by_ft or {}
+            opts.formatters_by_ft.svelte = { "prettier" }
+            return opts
+        end,
+    },
+}
