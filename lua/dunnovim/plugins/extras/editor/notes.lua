a8a0cbb6525ab74cefd96310f1088c228f5ceea8
teesh3rt <ilaylevy611@gmail.com>
2025-05-07 21:30:45 +0300

    fix: accidentally forgot to add keybinds

--- lua/dunnovim/plugins/extras/editor/notes.lua
+++ lua/dunnovim/plugins/extras/editor/notes.lua
@@ -1,6 +1,11 @@
 return {
     {
         "teesh3rt/zeekay.nvim",
+        keys = {
+            { "<leader>n", "", desc = "+notes" },
+            { "<leader>nn", "<cmd>ZeekayNewNote<cr>", desc = "Create a new note" },
+            { "<leader>fn", "<cmd>ZeekayPickNote<cr>", desc = "Find note" },
+        },
         opts = {},
     },
 }
